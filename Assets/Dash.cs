4ff65ac24018b6fd6fc0a49959fa5309ecad85b7
JoshSauter <sauterj@umich.edu>
2016-03-07 21:19:14 -0500

    Hmm, dashing

---
+++ Assets/Dash.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dash : Ability {
+	float dashTime = 0.1f;
+	float dashDistance = 4f;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+
+		cooldown = 2f;
+		activateKey = KeyCode.Q;
+	}
+
+	public override void Activate() {
+		base.Activate();
+
+		StartCoroutine(DashCoroutine());
+	}
+	IEnumerator DashCoroutine() {
+		float timeElapsed = 0;
+		while (timeElapsed < dashTime) {
+			timeElapsed += Time.deltaTime;
+			float percent = timeElapsed / dashTime;
+
+			thisPlayer.thisRigidbody.AddForce(thisPlayer.transform.forward * (dashDistance / dashTime), ForceMode.VelocityChange);
+
+			yield return 0;
+		}
+	}
+}
